afd41f278ffa1e1240e3d29fae89fd499c1a11ec
Florian Privé <florian.prive.21@gmail.com>
2017-09-13 17:38:06 +0200

    commiiiiiiiiiiiiiiiiiit

--- rrGBLUP.R
+++ rrGBLUP.R
@@ -1,3 +1,4 @@
+library(bigsnpr)
 # infos <- readRDS("training/sim1a.rds")
 infos <- readRDS("validation/sim2a.rds")
 
@@ -10,33 +11,30 @@ G2
 
 CHR <- infos$chromosome[ind.col]
 
+normalize <- function(x) {
+  qx <- ppoints(length(x))
+  qnorm(qx[rank(x)])
+} 
 cov_base <- cbind(normalize(infos$envi),
                   model.matrix(~as.factor(infos$pop)))
 cov_add <- NULL
 ind <- NULL
-G3 <- G2[]
-
+G3 <- scale(G2[])
 
 cov3 <- cbind(cov_base, cov_add) 
 
-ans <- mixed.solve(normalize(infos$phenotype1), Z = G3, X = cov3, SE = TRUE)
+ans <- rrBLUP::mixed.solve(normalize(infos$phenotype1), Z = G3, X = cov3, SE = TRUE,
+                           K = crossprod(G3))
 
 score <- ans$u / ans$u.SE
 length(score)
 pval <- pchisq(score^2, df = 1, lower.tail = FALSE)
 plot(-log10(pval))
 
-# gwas3 <- big_univLinReg(G2, normalize(infos$phenotype1), covar.train = cov3)
-# gwas3[is.na(gwas3$score), "score"] <- 0
-# gwas3.gc <- snp_gc(gwas3)
-
-
-# ind.keep <- snp_clumping(G2, CHR, S = -pval, size = 100, 
-#                          exclude = ind)
 
 new.ind <- which.min(pval)
 ind <- c(ind, new.ind)
-cov_add <- G2[, ind, drop = FALSE]
+cov_add <- scale(G2[, ind, drop = FALSE])
 G3[, new.ind] <- rnorm(1000)
 ind.col[ind]
 
