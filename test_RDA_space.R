21f316c2469fb5f796f993322a3823dcc1e0b7d6
JujuW <jwittische@gmail.com>
2017-09-13 10:59:33 +0200

    loooooolz

--- test_RDA_space.R
+++ test_RDA_space.R
@@ -16,14 +16,14 @@ scatter.dapc(dapc)
 saveRDS(clus$grp, file="gen_clusters.rds")
 
 # Combine coordinates
-spa<-cbind(x1$x,x1$y)
+spa<-cbind(x1$fam$x,x1$fam$y)
 # Create Moran's I Eigenvector Maps
 mem_pos<-dbmem(spa,MEM.autocor = "positive")
 # RDA with all MEM
-mem_null.rda<-rda(t(x$G),mem_pos)
+mem_null.rda<-rda(G,mem_pos)
 (mem_null.R2a<-RsquareAdj(mem_null.rda)$adj.r.squared)
 (mem_null.R2<-RsquareAdj(mem_null.rda)$r.squared)
 # Forward selection
-mem_null.fwd<-forward.sel(t(x$G),mem_pos, adjR2thresh = mem_null.R2, alpha=0.05, nperm=99)
+mem_null.fwd<-forward.sel(G,mem_pos, adjR2thresh = 5, alpha=0.05, nperm=99)
 mem_null.sign<-sort(functional_group1.MEMpos.fwd$order)
 mem_null.order<-as.data.frame(mem_pos[c(MEMpos.sign)])
